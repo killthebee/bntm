c77c1732ac9271db9ea45e6c9957775acec01ef0
killthebee <superhotfire447@yandex.ru>
2022-04-06 17:50:49 +0300

    implement total supply endpoint

--- nft_token/api/urls.py
+++ nft_token/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from nft_token.api.views import DummyView, CreateTokenView, TokenListView
+from nft_token.api.views import SupplyView, CreateTokenView, TokenListView
 
 app_name = "api"
 
 urlpatterns = [
-    path("dummy", DummyView.as_view(), name="sing_up"),
+    path("tokens/total_supply/", SupplyView.as_view(), name="total_supply"),
     path("tokens/create/", CreateTokenView.as_view(), name="create_token"),
     path("tokens/list/", TokenListView.as_view(), name="token_list"),
 ]
--- nft_token/api/views.py
+++ nft_token/api/views.py
@@ -4,11 +4,7 @@ from rest_framework.views import APIView
 
 from nft_token.models import Token
 from nft_token.api.serializers import CreateTokenSerializer, ListTokenSerializer
-
-
-class DummyView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({"hello": "world"})
+from bntm.utils import get_contract
 
 
 class CreateTokenView(generics.CreateAPIView):
@@ -22,3 +18,10 @@ class TokenListView(generics.ListAPIView):
 
     def get_queryset(self):
         return self.model.objects.all()
+
+
+class SupplyView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        contract = get_contract()
+        return Response({"result": contract.functions.totalSupply().call()})
